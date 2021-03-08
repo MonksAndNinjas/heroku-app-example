8b15eb75e279c5a139f9b52e0d37d6679b5bfd31
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-08 12:51:57 -0700

    Update App.js
    prepping for deletion of code

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 // connects to store and allows use of functions in /actions
 // I place fetch calls here because it avoids duplication of store arrays
 import { connect } from 'react-redux';
-//import { fetchBlogPosts } from './actions/fetch'; // Rails api, could change BlogPosts; no needed
-//import { fetchGitHubPosts } from './actions/fetch'; // monksandninjas.github.io
-//import { fetchProjects } from './actions/fetch'; // files in github
 import { fetchPhotos } from './actions/fetch';  // monksandninjas.github.io
-//import { fetchProfile } from './actions/fetch'; // Rails api
 
 class App extends React.Component {
   render() {
@@ -17,24 +13,30 @@ class App extends React.Component {
   }
   // fetch data from API's
   componentDidMount() {
-  //  this.props.fetchBlogPosts()
-  // this.props.fetchGitHubPosts()
-  //  this.props.fetchProjects()
     this.props.fetchPhotos()
-  //  this.props.fetchProfile()
   }
 }
 
 const mapStateToProps = state => {
   return({
-  //  postsData: state.posts,
-  //  gitPostsData: state.gitPosts,
-  //  projectsData: state.projects,
     photosData: state.photos,
-  //  profileData: state.profile
   })
 }
 
 export default connect(mapStateToProps, {fetchPhotos})(App);
 
+
+//code to be erased
 //fetchBlogPosts, fetchGitHubPosts, fetchProjects, fetchProfile 
+////import { fetchBlogPosts } from './actions/fetch'; // Rails api, could change BlogPosts; no needed
+//import { fetchGitHubPosts } from './actions/fetch'; // monksandninjas.github.io
+//import { fetchProjects } from './actions/fetch'; // files in github
+//import { fetchProfile } from './actions/fetch'; // Rails api
+  //  this.props.fetchBlogPosts()
+  // this.props.fetchGitHubPosts()
+  //  this.props.fetchProjects()
+  //  this.props.fetchProfile()
+//  //  postsData: state.posts,
+  //  gitPostsData: state.gitPosts,
+  //  projectsData: state.projects,
+  //  profileData: state.profile
