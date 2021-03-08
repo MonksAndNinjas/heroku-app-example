6f03a732b21963275d1b88a6979bcdef1e6ed654
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-08 13:00:40 -0700

    Update fetch.js
    delete

--- client/src/actions/fetch.js
+++ client/src/actions/fetch.js
@@ -1,48 +1,3 @@
-// fetches data from Rails API
-export function fetchBlogPosts() {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_POSTS' });
-
-    return fetch('api/v1/posts', {
-      accept: 'application/json',
-    }).then(response => response.json())
-      .then(posts => dispatch({ type: 'FETCH_BLOG_POSTS', payload: posts }))
-  }
-}
-// fetches data from Github API
-export function fetchGitHubPosts() {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_GIT_POSTS' });
-    // fetches all posts within directory
-    return fetch('https://api.github.com/repos/MonksAndNinjas/MonksAndNinjas.github.io/contents/_posts', {
-      accept: 'application/json',
-    }).then(response => response.json())
-      .then((posts) => {
-          let postPromises = posts.map((post) => {
-              dispatch({ type: 'LOADING_GIT_POST' })
-              // makes individual calls to retrieve data for a post
-              return fetch(post.git_url, {
-                accept: 'application/json',
-              }).then(response => response.json())
-                .then(post => dispatch({ type: 'FETCH_GITHUB_POST', payload: post }))
-          })
-          // makes sure all promises have been completed before continuing
-          return Promise.all(postPromises)
-        }).then(data => dispatch({ type: 'FETCH_GITHUB_POSTS', payload: data }))
-
-  }
-}
-// fetches project data from Rails API
-export function fetchProjects() {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_PROJECTS' });
-
-    return fetch('api/v1/projects',{
-      accept: 'application/json',
-    }).then(response => response.json())
-      .then(projects => dispatch({ type: 'FETCH_PROJECTS', payload: projects }))
-  }
-}
 // fetches photos of artwork from GITHUB
 export function fetchPhotos() {
   return (dispatch) => {
@@ -64,16 +19,5 @@ export function fetchPhotos() {
         // makes sure all promises have been complete before continuing
         return Promise.all(photoPromises)
       }).then(photos => dispatch({ type: 'FETCH_PHOTOS_DATA', payload: photos }))
-  }
-}
-// fetches profile data from Rails API
-export function fetchProfile() {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_PROFILE' });
-
-    return fetch('api/v1/profiles', {
-      accept: 'application/json',
-    }).then(response => response.json())
-      .then(profile => dispatch({ type: 'FETCH_PROFILE', payload: profile }))
   }
 }
