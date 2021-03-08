ad5102ab5197315dc0060a25e09bbe89d406596a
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-08 12:28:21 -0700

    Update fetch.js

--- client/src/actions/fetch.js
+++ client/src/actions/fetch.js
@@ -14,7 +14,7 @@ export function fetchGitHubPosts() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_GIT_POSTS' });
     // fetches all posts within directory
-    return fetch('https://api.github.com/repos/MonksAndNinjas/MonksAndNinjas.github.io/_posts', {
+    return fetch('https://api.github.com/repos/MonksAndNinjas/MonksAndNinjas.github.io/contents/_posts', {
       accept: 'application/json',
     }).then(response => response.json())
       .then((posts) => {
@@ -48,7 +48,7 @@ export function fetchPhotos() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_PHOTOS_DATA' });
     // fetches all photos within directory
-    return fetch('https://api.github.com/repos/MonksAndNinjas/MonksAndNinjas.github.io/img', {
+    return fetch('https://api.github.com/repos/MonksAndNinjas/MonksAndNinjas.github.io/contents/img', {
       accept: 'application/json',
     }).then(response => response.json())
       .then((data) => {
@@ -56,7 +56,7 @@ export function fetchPhotos() {
         let photoPromises = data.map((photos) => {
           dispatch({ type: 'LOADING_PHOTOS' })
           // makes individual calls to retrieve data for photo
-          return fetch('https://api.github.com/repos/MonksAndNinjas/MonksAndNinjas.github.io/img/' + photos.name, {
+          return fetch('https://api.github.com/repos/MonksAndNinjas/MonksAndNinjas.github.io/contents/img/' + photos.name, {
             accept: 'application/json',
           }).then(response => response.json())
             .then(photo => dispatch({ type: 'FETCH_PHOTOS', name: photos.name, payload: photo }))
