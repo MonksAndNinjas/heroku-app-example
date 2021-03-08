82891ebc51648ba381cd28a43fd09b23dbadfa5f
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-08 12:56:06 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,19 +24,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, {fetchPhotos})(App);
-
-
-//code to be erased
-//fetchBlogPosts, fetchGitHubPosts, fetchProjects, fetchProfile 
-////import { fetchBlogPosts } from './actions/fetch'; // Rails api, could change BlogPosts; no needed
-//import { fetchGitHubPosts } from './actions/fetch'; // monksandninjas.github.io
-//import { fetchProjects } from './actions/fetch'; // files in github
-//import { fetchProfile } from './actions/fetch'; // Rails api
-  //  this.props.fetchBlogPosts()
-  // this.props.fetchGitHubPosts()
-  //  this.props.fetchProjects()
-  //  this.props.fetchProfile()
-//  //  postsData: state.posts,
-  //  gitPostsData: state.gitPosts,
-  //  projectsData: state.projects,
-  //  profileData: state.profile
