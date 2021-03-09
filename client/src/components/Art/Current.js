4f1e6b4b4d191a6a3c0e2eb3776e8c6cbba5f111
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-09 12:27:21 -0700

    Update Current.js

--- client/src/components/Art/Current.js
+++ client/src/components/Art/Current.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+class Current extends React.Component {
+  
+  render() {
+    return(
+      <div>
+        <h1>Current</h1>
+      </div>
+    )
+  }
+}
+
+export default Current;
