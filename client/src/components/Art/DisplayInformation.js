e3c7e8e19791934909f78b0a7642940a7bed1e7a
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-09 11:01:10 -0700

    Update DisplayInformation.js
    added event handler for buttons

--- client/src/components/Art/DisplayInformation.js
+++ client/src/components/Art/DisplayInformation.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 class DisplayInformation extends React.Component {
+  handleClick(e) {
+    console.log(e);
+  }
+  
   render() {
     return(
       <div>
-      <button>Current</button>
-      <button>Past</button>
-      <button>Events</button>
-      <button>About</button>
-      <button>Contact</button>
+        <button onClick={() => this.handleClick('Current')} >Current</button>
+        <button onClick={() => this.handleClick('Past')} >Past</button>
+        <button onClick={() => this.handleClick('Events')} >Events</button>
+        <button onClick={() => this.handleClick('About')} >About</button>
+        <button onClick={() => this.handleClick('Contact')} >Contact</button>
       </div>
     )
   }
