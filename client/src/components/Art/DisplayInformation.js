492087ef783f2bfc1c304f8ebd536edc3e8c38d7
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-09 11:33:09 -0700

    Update DisplayInformation.js

--- client/src/components/Art/DisplayInformation.js
+++ client/src/components/Art/DisplayInformation.js
@@ -16,11 +16,21 @@ class DisplayInformation extends React.Component {
   render() {
     return(
       <div>
-        <button onClick={() => this.handleClick('Current')} >Current</button>
-        <button onClick={() => this.handleClick('Past')} >Past</button>
-        <button onClick={() => this.handleClick('Events')} >Events</button>
-        <button onClick={() => this.handleClick('About')} >About</button>
-        <button onClick={() => this.handleClick('Contact')} >Contact</button>
+        <div>
+          <button onClick={() => this.handleClick('Current')} >Current</button>
+          <button onClick={() => this.handleClick('Past')} >Past</button>
+          <button onClick={() => this.handleClick('Events')} >Events</button>
+          <button onClick={() => this.handleClick('About')} >About</button>
+          <button onClick={() => this.handleClick('Contact')} >Contact</button>
+        </div>
+
+         {
+           this.state.showMe === 'Current' (<h1>'Current'</h1>) : null
+           this.state.showMe === 'Past' (<h1>'Past'</h1>) : null  
+           this.state.showMe === 'Events' (<h1>'Events'</h1>) : null     
+           this.state.showMe === 'About' (<h1>'About'</h1>) : null  
+           this.state.showMe === 'Contact' (<h1>'Contact'</h1>) : null  
+         }                                    
       </div>
     )
   }
