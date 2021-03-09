24383ff7c2b78a7ef92df5c8caebe5bee5d53992
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-09 14:08:43 -0700

    Update Events.js

--- client/src/components/Art/Events.js
+++ client/src/components/Art/Events.js
@@ -6,6 +6,32 @@ class Events extends React.Component {
     return(
       <div>
         <h1>Events</h1>
+     
+        <h2>Current</h2>
+        
+        <ul>
+         <li>Tierra y Fuego</li>
+         <li>Chataqua Park Winterfest</li>
+        <ul>
+     
+        <h2>Upcoming</h2>
+          
+        <ul>
+         <li>North Boulder Absurdism show - April 2nd</li>
+        </ul>
+     
+        <h2>Past</h2>
+     
+        <ul>
+         <li>Pet Portraits through OpenStudios - February - Boulder</li>
+         <li>Museum of Boulder OpenStudios show - October - Boulder</li>
+         <li>Avalon Drive Thru Art Show - September - Boulder</li>
+         <li>OpenStudios Outdoor Show - June</li>
+         <li>Art and Live Music - 2014 - Redwood City</li>
+         <li>Chicano Heritage Month - 2013 - Redwood City</li>
+         <li>Big Umbrella Studios Monthly Art Shows - (2011 - 2012) - San Francisco</li>
+        </ul>
+     
       </div>
     )
   }
