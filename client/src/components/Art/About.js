e643293bf340c76842de1a20939d02f558f88e4a
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-09 13:06:18 -0700

    Update About.js

--- client/src/components/Art/About.js
+++ client/src/components/Art/About.js
@@ -5,7 +5,26 @@ class About extends React.Component {
   render() {
     return(
       <div>
-        <h1>About</h1>
+        <h1>About Joseph Jimenez</h1>
+
+         <br/>
+
+        <p>
+           Manga and Catholic aesthetics, dark themes intertwined with hopeful narratives. 
+           Born from Mexican and El Salvadoran immigrant parents, I grew up in Hollywood, 
+           Los Angeles during a time that included the LA riots, ‘94 earthquake, and the 
+           redefining of chicano culture. These tough barrios formed unique styles to 
+           properly represent these narratives and identities. It was a crazy period with 
+           frequent violence, demanding a rough attitude mixed in with some heavy doses of 
+           catholicism to keep you from falling into darkness. Hope... that is. Yearning 
+           for an escape, for something outside these omnipresent themes, I quickly buried 
+           myself in anime and mangas. My style overtime coalesced to form a unique voice 
+           that pays homage to my past evoking darkness, hopefulness and a sense of 
+           playfulness. In the end My work is in constant pursuit of balance, despite the 
+           world’s pull towards extremes. Now living in Boulder, CO I aim to meld my 
+           experiences with what it means to be a Boulderite. Having already lived here 
+           before for six years, I have a wealth of locations and experiences to pull from. 
+       </p>
       </div>
     )
   }
