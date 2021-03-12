ff5f85485cb6e482589c1b6e4b58fe9a8df58d34
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-12 14:30:18 -0700

    Update About.js

--- client/src/components/Art/About.js
+++ client/src/components/Art/About.js
@@ -5,7 +5,7 @@ class About extends React.Component {
   render() {
     return(
       <div className="aboutContainer" >
-        <h1>About Joseph Jimenez</h1>
+        <h2>Joseph Jimenez</h2>
 
          <br/>
 
