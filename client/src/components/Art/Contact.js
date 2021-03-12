1d83f0e9ba4ad6557367e75afc0617b926e8faa9
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-12 15:48:39 -0700

    Update Contact.js

--- client/src/components/Art/Contact.js
+++ client/src/components/Art/Contact.js
@@ -5,7 +5,7 @@ class Contact extends React.Component {
   render() {
     return(
       <div className="contactContainer" >
-        <h2>Contact</h2>
+        <h1>Contact</h1>
         
         <p>7202201556</p>
         <a href={"mailto:" + 'Joseph@MonksAndNinjas.com'}>Joseph@MonksAndNinjas.com</a>      
