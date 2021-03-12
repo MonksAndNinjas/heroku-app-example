47bf19b5940804c93a9ba2358129feadc4e29f70
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-12 12:27:49 -0700

    Update Events.js

--- client/src/components/Art/Events.js
+++ client/src/components/Art/Events.js
@@ -4,33 +4,30 @@ class Events extends React.Component {
   
   render() {
     return(
-      <div>
-        <h1>Events</h1>
+      <div className="eventContainer>
+        <h1>Exhibitions and Events</h1>
      
         <h2>Current</h2>
         
-        <ul>
-         <li>Tierra y Fuego</li>
-         <li>Chataqua Park Winterfest</li>
-        </ul>
+ 
+        <p>Tierra y Fuego Taqueria - North Boulder</p>
+        <p>Chataqua Park Winterfest - Boulder</p>
      
         <h2>Upcoming</h2>
           
-        <ul>
-         <li>North Boulder Absurdism show - April 2nd</li>
-        </ul>
+        <p>North Boulder Absurdism show - April 2nd</p>
+        <p>Plein Air Festival - Boulder - June 4th - 11th</p> 
      
         <h2>Past</h2>
      
-        <ul>
-         <li>Pet Portraits through OpenStudios - February - Boulder</li>
-         <li>Museum of Boulder OpenStudios show - October - Boulder</li>
-         <li>Avalon Drive Thru Art Show - September - Boulder</li>
-         <li>OpenStudios Outdoor Show - June</li>
-         <li>Art and Live Music - 2014 - Redwood City</li>
-         <li>Chicano Heritage Month - 2013 - Redwood City</li>
-         <li>Big Umbrella Studios Monthly Art Shows - (2011 - 2012) - San Francisco</li>
-        </ul>
+        <p>Pet Portraits through OpenStudios - February 2021- Boulder</p>
+        <p>Museum of Boulder OpenStudios Fall show - October 2020- Boulder</p>
+        <p>Avalon Drive Thru Art Show - September 2020 - Boulder</p>
+        <p>OpenStudios Outdoor Show - June 2020</p>
+        <p>Private Show and String Quartet - 2014 - Redwood City</p>
+        <p>Chicano Heritage Month at Canada College - 2013 - Redwood City</p>
+        <p>Big Umbrella Studios - (2011 - 2012) - San Francisco</p>
+        <p>Rembrandt Yard - Various Artist - 2011 - Boulder</p>
      
       </div>
     )
