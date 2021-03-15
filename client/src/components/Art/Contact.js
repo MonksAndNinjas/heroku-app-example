56de317074774bcc5d5f4962d94c4c95e570e753
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-14 22:37:55 -0600

    Update Contact.js

--- client/src/components/Art/Contact.js
+++ client/src/components/Art/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import phoneSymbol from '../images/phoneSymbol.svg';
+import emailSymbol from '../images/mailsymbol.svg';
+
 class Contact extends React.Component {
   
   render() {
@@ -7,11 +10,11 @@ class Contact extends React.Component {
       <div className="contactContainer" >
         <h1>Contact</h1>
         
-        <a href="tel:+7202201556">Tel: 7202201556</a>
+        <a href="tel:+7202201556"><img id="phone" style={{width: "100%"}} src={phoneSymbol} alt="telephone" /></a>
         
         <br/>
         
-        <a href={"mailto:" + 'Joseph@MonksAndNinjas.com'}>Email: Joseph@MonksAndNinjas.com</a>      
+        <a href={"mailto:" + 'Joseph@MonksAndNinjas.com'}><img id="email" style={{width: "100%"}} src={emailSymbol} alt="email" /></a>      
       </div>
     )
   }
