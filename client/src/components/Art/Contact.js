12969fde89c11068e9f2ee1f8f2cf00bc0e22570
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-14 22:45:27 -0600

    Update Contact.js

--- client/src/components/Art/Contact.js
+++ client/src/components/Art/Contact.js
@@ -10,11 +10,8 @@ class Contact extends React.Component {
       <div className="contactContainer" >
         <h1>Contact</h1>
         
-        <a href="tel:+7202201556"><img id="phone" style={{width: "100%"}} src={phoneSymbol} alt="telephone" /></a>
-        
-        <br/>
-        
-        <a href={"mailto:" + 'Joseph@MonksAndNinjas.com'}><img id="email" style={{width: "100%"}} src={emailSymbol} alt="email" /></a>      
+        <a href="tel:+7202201556"><img id="phone" style={{width: "7%"}} src={phoneSymbol} alt="telephone" /></a>
+        <a href={"mailto:" + 'Joseph@MonksAndNinjas.com'}><img id="email" style={{width: "7%"}} src={emailSymbol} alt="email" /></a>      
       </div>
     )
   }
