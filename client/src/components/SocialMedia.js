de705090b17f2b7d1373be932a5e511f19d09ff4
MonksAndNinjas <blackbeltdiaries@gmail.com>
2021-03-17 19:10:19 -0600

    Update SocialMedia.js

--- client/src/components/SocialMedia.js
+++ client/src/components/SocialMedia.js
@@ -11,6 +11,8 @@ class SocialMedia extends React.Component {
       <div id="socialMedia">
         <SocialIcon style={{ overflow: 'visible', height: '25px', width: '25px' }} url="https://www.instagram.com/monksandninjas/?hl=en" />
         <SocialIcon style={{ overflow: 'visible', height: '25px', width: '25px' }} url="https://www.linkedin.com/in/joseph-jimenez-31794711b/" />
+        <SocialIcon style={{ overflow: 'visible', height: '25px', width: '25px' }} url="https://twitter.com/monksandninjas" />
+        <SocialIcon style={{ overflow: 'visible', height: '25px', width: '25px' }} url="https://www.facebook.com/Monks-And-Ninjas-315010412179306" />
       </div>
     );
   }
