9e8899931c39c4aa3407df5de4873e3d02e3e6f5
MonksAndNinjas <blackbeltdiaries@gmail.com>
2022-04-05 11:47:37 -0600

    updated events

--- client/src/components/Art/Events.js
+++ client/src/components/Art/Events.js
@@ -11,19 +11,23 @@ class Events extends React.Component {
         
  
         <p>Tierra y Fuego Taqueria - North Boulder</p>
-        <p>Chataqua Park Winterfest - Boulder</p>
+        <p>Voces Vivas - Museum of Boulder</p>
      
         <h2>Upcoming</h2>
           
-        <p>North Boulder Absurdism show - April 2nd</p>
-        <p>Plein Air Festival - Boulder - June 4th - 11th</p> 
+        <p>Ozo Coffee - June 2022 - Boulder (Pearl & Arapahoe)</p>
+        <p>STEAMY Cantina - Fall 2022 - Fiske Planitarium</p>
      
         <h2>Past</h2>
      
-        <p>Pet Portraits through OpenStudios - February 2021- Boulder</p>
-        <p>Museum of Boulder OpenStudios Fall show - October 2020- Boulder</p>
+        <p>Ironton Distillery Solo Show - March 2022 - Denver</p>
+        <p>Ozo Coffee - January 2022 - Boulder</p>
+        <p>NoBo arts district Absurdism Show - April 2021 -  Boulder</p>
+        <p>Pet Portraits through OpenStudios - February 2021 - Boulder</p>
+        <p>Chataqua Park Winterfest - Winter 2020 - Boulder</p>
+        <p>Museum of Boulder OpenStudios Fall show - October 2020 - Boulder</p>
         <p>Avalon Drive Thru Art Show - September 2020 - Boulder</p>
-        <p>OpenStudios Outdoor Show - June 2020</p>
+        <p>OpenStudios Outdoor Show - June 2020 - Boulder</p>
         <p>Private Show and String Quartet - 2014 - Redwood City</p>
         <p>Chicano Heritage Month at Canada College - 2013 - Redwood City</p>
         <p>Big Umbrella Studios - (2011 - 2012) - San Francisco</p>
